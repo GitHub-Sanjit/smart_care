d7b0d5c4cb120fc8901e3576e48e23e90b96ac69
Sanjit Sarkar <sanjit.est14@gmail.com>
2024-01-12 14:04:26 +0600

    updated

--- smart_care/settings.py
+++ smart_care/settings.py
@@ -31,7 +31,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = [
-    'https://smart-care-helt.onrender.com/', 'https://*.127.0.0.1']
+    'https://*.smart-care-helt.onrender.com/', 'https://*.127.0.0.1']
 
 
 # Application definition
